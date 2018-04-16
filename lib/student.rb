7b6955862d6d0e4ddf4482e3eaddaab2e585746f
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-16 18:24:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -77,4 +77,12 @@ class Student
       self.new_from_db(row)
     end
   end
+
+  def self.first_X_students_in_grade_10
+    sql = <<-SQL
+    SELECT COUNT(*)
+    FROM students
+    WHERE grade = 10
+    SQL
+    DB[:conn].execute(sql)
 end
