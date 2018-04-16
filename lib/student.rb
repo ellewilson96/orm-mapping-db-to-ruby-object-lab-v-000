4304459d9169f4442681f07f36d0d13ecbe5ddc4
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-16 18:12:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,4 +56,14 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+    SELECT COUNT(*)
+    FROM orders
+    WHERE grade = 9
+    SQL
+
+    DB[:conn].execute(sql))
+
 end
